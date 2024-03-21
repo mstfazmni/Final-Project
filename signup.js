adec62246418e32eb872d059dbca9f84aa4f31b4
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-03-22 01:44:24 +0530

    signup.js

---
+++ signup.js
@@ -0,0 +1,37 @@
+let landlordEl = document.getElementById("landlord");
+let tenentEl = document.getElementById("tenent");
+let signupBtn = document.getElementById("signUpBtn");
+let landlord;
+let tenent;
+
+
+landlordEl.addEventListener("change", ()=>{
+  if (landlordEl.checked) {
+    console.log('Landlord selected');
+    landlord = true;
+    tenent = false;
+  }
+  else{
+    landlord = false;
+  }
+})
+
+tenentEl.addEventListener('change', () => {
+  if (tenentEl.checked) {
+    console.log('Tenant selected');
+    tenent = true;
+    landlord = false;
+  }
+  else{
+    tenent = false;
+  }
+});
+
+signupBtn.addEventListener("click", ()=>{
+  if(landlord == true){
+    window.location.href = '/project/landlord.html';
+  }
+  else if(tenent == true){
+    window.location.href = '/project/index.html'
+  }
+})

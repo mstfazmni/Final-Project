2e1ffcfd59d261b04984f7fe3d53ca77e680c9c6
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-03-22 00:06:48 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const city = ["Calgary", "Edmonton", "Red Deer", "Canmore"];
-const cityLinks = ["calgary.jpg", "edmonton.jpg", "red deer.jpg", "canmore.jpg"];
+
+const albertaCities = [
+    { name: "Calgary", image: "calgary.jpg" },
+    { name: "Edmonton", image: "edmonton.jpg" },
+    { name: "Red Deer", image: "red deer.jpg" },
+    { name: "Canmore", image: "canmore.jpg" },
+];
+
+const ontarioCities = [
+    { name: "Toronto", image: "toronto.jpg" },
+    { name: "London", image: "london.jpg" },
+    { name: "Ottawa", image: "ottawa.jpg" },
+    { name: "Hamilton", image: "hamilton.jpg" },
+];
+
+const britishCities = [
+    { name: "Vancouver", image: "vancouver.jpg" },
+    { name: "Victoria", image: "victoria.jpg" },
+    { name: "Surrey", image: "surrey.jpg" },
+    { name: "Burnaby", image: "burnaby.jpg" },
+];
+
 let boxesGenerated = false;
 
-function ShowingCity() {
+function ShowingCity(cities) {
 
     // Get the reference to the city-div element only once outside the loop
     let cityVar = document.getElementById("city-div");
+    let provContainer = document.getElementById("back-div");
 
     if (!boxesGenerated) {
+
+        
         // Create a container div to hold all the city cards
         let containerDiv1 = document.createElement("div");
         containerDiv1.className = "row my-container1";
@@ -15,33 +38,38 @@ function ShowingCity() {
         let containerDiv2 = document.createElement("div");
         containerDiv2.className = "row my-container2";
 
-        for (let i = 0; i < city.length; i++) {
+        for (let i = 0; i < cities.length; i++) {
             // Create a new div for each city and append it to containerDiv
             let cityDiv = document.createElement("div");
             cityDiv.className = "col city-card"; // Add a specific class for styling
             cityDiv.innerHTML = `
-                <a href="property.html?city=${city[i]}" target="_blank" class="a-city">
-                <img src="${cityLinks[i]}" class="card-img-city" alt="${city[i]}">
+                <a href="property.html?city=${cities[i].name}" target="_blank" class="a-city">
+                <img src="${cities[i].image}" class="card-img-city" alt="${cities[i].name}">
                 <div class="card-body">
-                    <h5 class="card-title">${city[i]}</h5>
+                    <h5 class="card-title">${cities[i].name}</h5>
                 </div>
                 </a>
             `;
-
+            
             // Append the new cityDiv to containerDiv
-            if (i < city.length /2 ){
+            if (i < cities.length /2 ){
                 containerDiv1.appendChild(cityDiv);
             }else {
                 containerDiv2.appendChild(cityDiv);
             }
             
-            
-            //containerDiv.appendChild(cityDiv);
-
         }
+       
+        //clear everything
+        cityVar.innerHTML = '';
+
         // Append the containerDiv to cityVar
         cityVar.appendChild(containerDiv1);
         cityVar.appendChild(containerDiv2);
+
+        //add back link
+        provContainer.innerHTML = `<a href="index.html" class="back-div">Back</a>`;
+
         boxesGenerated = true;
         
     }

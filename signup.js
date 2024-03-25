f86af352411856e3c83bac1a66d8021d11711a0c
Deepak Poly <119725894+Daddy2424@users.noreply.github.com>
2024-03-25 07:14:15 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,6 +1,7 @@
 let landlordEl = document.getElementById("landlord");
 let tenentEl = document.getElementById("tenent");
 let signupBtn = document.getElementById("signUpBtn");
+let form = document.getElementById("form");
 let landlord;
 let tenent;
 
@@ -27,11 +28,31 @@ tenentEl.addEventListener('change', () => {
   }
 });
 
-signupBtn.addEventListener("click", ()=>{
-  if(landlord == true){
+let errorExist = false;
+
+signupBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let firstName = document.getElementById("FirstName").value;
+  let lastName = document.getElementById("LastName").value;
+  let phone = document.getElementById("Phone").value;
+  let email = document.getElementById("Email").value;
+  let password = document.getElementById("Pass").value;
+
+  if (landlord && firstName && lastName && !isNaN(phone) && email && password) {
     window.location.href = 'landlord.html';
+  } else if (tenent && firstName && lastName && !isNaN(phone) && email && password) {
+    window.location.href = 'index.html';
+  } else {
+
+    if (errorExist == false){
+      let msg = document.createElement('p');
+      msg.textContent = "Please complete the form !"
+      msg.style.color = "red";
+      errorExist = true;
+      form.appendChild(msg);
+    }else if(errorExist == true){
+    }
+    
   }
-  else if(tenent == true){
-    window.location.href = 'index.html'
-  }
-})
+});
